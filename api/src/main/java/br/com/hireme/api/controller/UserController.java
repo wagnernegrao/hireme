66b792441de7ef25b2234e3c23b2e562f37d3af4
wagnernegrao <wagnerfelidre@gmail.com>
2021-03-11 10:18:59 -0300

    Add: was created scope to creation of method

--- api/src/main/java/br/com/hireme/api/controller/UserController.java
+++ api/src/main/java/br/com/hireme/api/controller/UserController.java
@@ -3,15 +3,16 @@ package br.com.hireme.api.controller;
 import br.com.hireme.api.model.User;
 import br.com.hireme.api.repository.UserRepository;
 import br.com.hireme.api.service.dto.UserDto;
+import org.apache.tomcat.util.http.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -24,9 +25,22 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public ResponseEntity<List<UserDto>>  findAllUser() {
+    public ResponseEntity<List<UserDto>> findAllUser() {
         log.debug("REST request to all users");
         List<User> users = userRepository.findAll();
         return ResponseEntity.ok().body(UserDto.toDto(users));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> findByUserId(@PathVariable Long id) {
+        log.debug("REST request to id: {}", id);
+        Optional<User> user = userRepository.findById(id);
+
+        return user
+                .map(value -> ResponseEntity
+                        .ok()
+                        .body(UserDto.toDto(value)))
+                        .orElseGet(() -> ResponseEntity.notFound().build());
+
     }
 }
