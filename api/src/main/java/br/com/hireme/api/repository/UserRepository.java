dbc3cd58ccbc1af0eb40f9989e95bb29f1ab313f
wagnernegrao <wagnerfelidre@gmail.com>
2021-03-11 10:18:18 -0300

    Remove: was removed method findByOne

--- api/src/main/java/br/com/hireme/api/repository/UserRepository.java
+++ api/src/main/java/br/com/hireme/api/repository/UserRepository.java
@@ -3,5 +3,7 @@ package br.com.hireme.api.repository;
 import br.com.hireme.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
