c45bebbb93e9ac215e290dd92c5129634f8e5d73
wagnernegrao <wagnerfelidre@gmail.com>
2021-03-16 19:55:55 -0300

    Add: new updates

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user(email, password, photo, contractor, service_provider)
+INSERT INTO USUARIO(EMAIL, SENHA, FOTO, CONTRATANTE, PRESTADOR_SERVICO)
 VALUES ('wagner@gmail.com', '$2a$10$udRPN4P08e18Mrh.a5mWQOLff.Yyjh7xrIq14J48Q.jARx7sN091y', NULL, true, false);
 
-INSERT INTO contractor(user_id, name, address, cnpj)
-VALUES (1, 'wagner', 'localhost', 123456789);
+INSERT INTO CONTRATANTE(USUARIO_ID, NOME, ENDERECO, CNPJ)
+VALUES (1, 'wagner', 'localhost', 12345671234567);
