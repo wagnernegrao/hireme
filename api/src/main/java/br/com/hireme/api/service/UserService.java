2d77e75c3f574b1bb9fa4d2a4580f9af1c92f10a
wagnernegrao <wagnerfelidre@gmail.com>
2021-03-16 19:59:46 -0300

    Add: was added method

--- api/src/main/java/br/com/hireme/api/service/UserService.java
+++ api/src/main/java/br/com/hireme/api/service/UserService.java
@@ -1,11 +1,27 @@
 package br.com.hireme.api.service;
 
-import br.com.hireme.api.service.dto.UserContractorDto;
+import br.com.hireme.api.domain.User;
 import br.com.hireme.api.service.dto.UserDto;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface UserService {
+    /*
+     * Find all users
+     * */
+    List<UserDto> findAll();
 
-    UserContractorDto findOne(Long id);
+    /*
+    * Find the user using ID, if this use not found is
+    * return Not Found Exception.
+    * */
+    User findOne(Long id);
+
+    /*
+    * Save a user, but find if user already exists,
+    * if find a user then return Not Accepted
+    * if not found then create a user.
+    * */
+    User save(User user);
+
 }
