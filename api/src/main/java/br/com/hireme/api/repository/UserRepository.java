7a3798586c68e408d49b8287ab7d995c4b5ee6e8
wagnernegrao <wagnerfelidre@gmail.com>
2021-03-16 19:59:09 -0300

    Add: was added method

--- api/src/main/java/br/com/hireme/api/repository/UserRepository.java
+++ api/src/main/java/br/com/hireme/api/repository/UserRepository.java
@@ -3,5 +3,8 @@ package br.com.hireme.api.repository;
 import br.com.hireme.api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
