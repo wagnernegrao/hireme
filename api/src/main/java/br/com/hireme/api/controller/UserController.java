693f216ff9965d4a2e01562010b5381638d246d3
wagnernegrao <wagnerfelidre@gmail.com>
2021-03-16 20:00:35 -0300

    Add: was added method to create a user

--- api/src/main/java/br/com/hireme/api/controller/UserController.java
+++ api/src/main/java/br/com/hireme/api/controller/UserController.java
@@ -1,20 +1,22 @@
 package br.com.hireme.api.controller;
 
+import br.com.hireme.api.domain.Contractor;
 import br.com.hireme.api.domain.User;
-import br.com.hireme.api.repository.ContractorRepository;
-import br.com.hireme.api.repository.UserRepository;
+import br.com.hireme.api.service.ContractorService;
 import br.com.hireme.api.service.UserService;
 import br.com.hireme.api.service.dto.UserContractorDto;
 import br.com.hireme.api.service.dto.UserDto;
+import br.com.hireme.api.service.dto.form.UserContractorForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,26 +27,25 @@ public class UserController {
     private Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ContractorRepository contractorRepository;
+    private ContractorService contractorService;
 
     @Autowired
     private UserService userService;
 
-
     @GetMapping
     public ResponseEntity<List<UserDto>> findAllUser() {
         log.debug("REST request to all users");
-        List<User> users = userRepository.findAll();
-        return ResponseEntity.ok().body(UserDto.toDto(users));
+        return ResponseEntity.ok().body(userService.findAll());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserContractorDto> findByUserId(@PathVariable Long id) {
-        log.debug("REST request to id: {}", id);
-        UserContractorDto user = userService.findOne(id);
-        return ResponseEntity.ok().body(user);
+    @PostMapping
+    @Transactional
+    public ResponseEntity<UserContractorDto> createUser(@Valid @RequestBody UserContractorForm userForm, UriComponentsBuilder builder) {
+        log.debug("REST request to userForm: {}", userForm);
+        User user = userService.save(userForm.toUser());
+        Contractor contractor = contractorService.save(userForm.toContractor(userService, user));
+        URI uri = builder.path("/api/users/{id}").buildAndExpand(contractor.getId()).toUri();
+
+        return ResponseEntity.ok().body(new UserContractorDto(user, contractor));
     }
 }
