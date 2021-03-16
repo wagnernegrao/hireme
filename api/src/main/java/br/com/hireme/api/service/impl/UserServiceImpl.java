daa8306744e8ae99ff7885fa6164b5a5385b50aa
wagnernegrao <wagnerfelidre@gmail.com>
2021-03-16 20:00:08 -0300

    Add: was added method

--- api/src/main/java/br/com/hireme/api/service/impl/UserServiceImpl.java
+++ api/src/main/java/br/com/hireme/api/service/impl/UserServiceImpl.java
@@ -1,17 +1,19 @@
 package br.com.hireme.api.service.impl;
 
-import br.com.hireme.api.handler.exception.NotFoundException;
-import br.com.hireme.api.domain.Contractor;
 import br.com.hireme.api.domain.User;
+import br.com.hireme.api.handler.exception.NotAcceptableException;
+import br.com.hireme.api.handler.exception.NotFoundException;
 import br.com.hireme.api.repository.ContractorRepository;
 import br.com.hireme.api.repository.UserRepository;
 import br.com.hireme.api.service.UserService;
-import br.com.hireme.api.service.dto.UserContractorDto;
+import br.com.hireme.api.service.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -25,12 +27,25 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public UserContractorDto findOne(Long id) {
-        Contractor contractor = contractorRepository.findById(id).orElseThrow(() -> new NotFoundException("Not found by id: " + id));
-        
-        Optional<User> user = userRepository.findById(contractor.getUserId().getId());
-        
-        return UserContractorDto.toDto(user.get(), contractor);
-       
+    public User findOne(Long id) {
+        return userRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Not found by id: " + id));
     }
+
+    @Override
+    public User save(User user) {
+        Optional<User> userEmail = userRepository.findByEmail(user.getEmail());
+
+        if (userEmail.isPresent()) userEmail.orElseThrow(() -> new NotAcceptableException("Not Acceptable, email in use " + userEmail.get().getEmail()));
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<UserDto> findAll() {
+        List<User> user = userRepository.findAll();
+        return user.stream().map(UserDto::toDto).collect(Collectors.toList());
+    }
+
 }
