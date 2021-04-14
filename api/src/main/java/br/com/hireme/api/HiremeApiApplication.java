d5063b677fbca1959849f8a326f10e2a7b70ca15
wagnernegrao <wagnerfelidre@gmail.com>
2021-04-13 22:32:06 -0300

    Add: was added anotation to enable caching in methods

--- api/src/main/java/br/com/hireme/api/HiremeApiApplication.java
+++ api/src/main/java/br/com/hireme/api/HiremeApiApplication.java
@@ -2,8 +2,10 @@ package br.com.hireme.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class HiremeApiApplication {
 
 	public static void main(String[] args) {
