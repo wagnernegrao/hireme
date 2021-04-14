ff0f5f3e649018fd63cce6fd41a731e06ae0ea77
wagnernegrao <wagnerfelidre@gmail.com>
2021-04-13 22:39:35 -0300

    Add: was added new methods to search

--- api/src/main/java/br/com/hireme/api/repository/ContractorRepository.java
+++ api/src/main/java/br/com/hireme/api/repository/ContractorRepository.java
@@ -1,7 +1,21 @@
 package br.com.hireme.api.repository;
 
 import br.com.hireme.api.domain.Contractor;
-import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.hireme.api.service.dto.FullContractorDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ContractorRepository extends JpaRepository<Contractor, Long> {
+import java.util.Optional;
+
+public interface ContractorRepository extends PagingAndSortingRepository<Contractor, Long> {
+
+//    @Query(value = "SELECT c.id, c.nome , u.foto, u.email, c.cnpj, c.endereco FROM contratante c INNER JOIN usuario u ON c.usuario_id = u.id", nativeQuery = true)
+//    Page<FullContractorDto> allContractors(Pageable pageable);
+    @Query(value = "SELECT NEW br.com.hireme.api.service.dto.FullContractorDto(c.id, c.name, u.photo, u.email, c.cnpj, c.address) " +
+            "FROM Contractor c INNER JOIN User u ON c.userId = u.id")
+    Page<FullContractorDto> allContractors(Pageable pageable);
+
+    Optional<Contractor> findByCnpj(String cnpj);
 }
--- api/src/main/java/br/com/hireme/api/repository/UserRepository.java
+++ api/src/main/java/br/com/hireme/api/repository/UserRepository.java
@@ -2,9 +2,10 @@ package br.com.hireme.api.repository;
 
 import br.com.hireme.api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
