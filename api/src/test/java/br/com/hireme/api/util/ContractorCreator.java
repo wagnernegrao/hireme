bdc2f9a566d6dc53620dc6af348f0a2621fe029e
wagnernegrao <wagnerfelidre@gmail.com>
2021-04-23 10:01:20 -0300

    Add: was added more features

--- api/src/test/java/br/com/hireme/api/util/ContractorCreator.java
+++ api/src/test/java/br/com/hireme/api/util/ContractorCreator.java
@@ -2,16 +2,27 @@ package br.com.hireme.api.util;
 
 import br.com.hireme.api.domain.Contractor;
 import br.com.hireme.api.domain.User;
-import br.com.hireme.api.service.dto.FullContractorDto;
+import br.com.hireme.api.service.dto.ContractorCompleteDto;
 
 public class ContractorCreator {
 
-    public FullContractorDto createContractorFiltered() {
-        FullContractorDto contractor = new FullContractorDto(99L, "contractor", "photo".getBytes(), "contractor@gmail.com", "00.000.000/0001-00", "address");
+    public ContractorCompleteDto createContractorFiltered() {
+        ContractorCompleteDto contractor = new ContractorCompleteDto(99L, "contractor", "photo".getBytes(), "contractor@gmail.com", "00.000.000/0001-00", "address");
         return contractor;
     }
 
     public Contractor createValidContractor(User user) {
+        return new Contractor(user,
+                "Contractor valid",
+                "110.000.000/0001-00",
+                "Address");
+    }
+
+    public Contractor createContractor() {
+        User user = new User("userValid.@email.com",
+                "12345",
+                "photo".getBytes());
+
         return new Contractor(user,
                 "Contractor valid",
                 "110.000.000/0001-00",
