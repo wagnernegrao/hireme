b7e878c287850ad2a35f7ef655ebccfed3fac613
wagnernegrao <wagnerfelidre@gmail.com>
2021-04-23 10:00:46 -0300

    Modify: was modified a import

--- api/src/test/java/br/com/hireme/api/repository/ContractorRepositoryTest.java
+++ api/src/test/java/br/com/hireme/api/repository/ContractorRepositoryTest.java
@@ -7,14 +7,11 @@ import br.com.hireme.api.util.UserCreator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @DisplayName("Test for contractor repository")
 class ContractorRepositoryTest {
